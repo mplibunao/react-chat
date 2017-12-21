bf0a3882ebe00d5f838910960d93bf1c236532d2
mplibunao <mark.libunao@shoresuite.com>
2017-12-21 13:16:48 +0800

    First commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log(0);
+    this.ws = new WebSocket("ws://localhost:8000/ws");
+    // this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+    console.log(1);
+    this.ws.onmessage = (e) => {
+      console.log(2);
+      //e.data
+      console.log('e.data: ', e.data);
+      //this.setState({ message:  })
+    }
+
+    this.ws.onOpen = (e) => {
+      console.log(3);
+    }
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>Hi</div>
     );
   }
 }
