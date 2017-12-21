4c370bc1dd16305f0aa3e3d6d6b3f4cc4df6258e
mplibunao <mark.libunao@shoresuite.com>
2017-12-21 21:24:08 +0800

    Chatcontent component

---
+++ src/components/ChatContent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export class ChatContent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    createMarkup(markupString) {
+        return {
+            _html: markupString,
+        }
+    }
+    render() { 
+        return (
+            <div className="row">
+                <div className="col s12">
+                    <div className="card horizontal">
+                        <div id="chat-messages"
+                            className="card-content"
+                            dangerouslySetInnerHTML={this.createMarkup(this.props.html)}
+                        >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+ 
+export default ChatContent;
